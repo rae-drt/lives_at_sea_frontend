20f348944142e02539819555d7b5c18ba74b42d6
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-02-17 14:45:25 +0000

    Add remaining person data
    Add the rest of the person data (not just services).
    
    Create a wrapper component to display the services and the new
    person data.
    
    Update page.tsx to display this.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import ServiceTable from "./servicetable";
+import ServiceRecord from "./servicerecord";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col-2 gap-8 row-start-2 items-center sm:items-start">
-        <ServiceTable/>
-        <ServiceTable/>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ServiceRecord/>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
