1810b13df8fe7f43c27547d8b33b0005bec8d04d
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-02-17 15:21:52 +0000

    Make outer margins narrower
    Works a bit better on narrower screens

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ServiceRecord from "./servicerecord";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ServiceRecord/>
       </main>
