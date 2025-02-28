272051d1bc034497dda9e07f99f873186c5f0004
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-02-28 11:58:16 +0000

    Controls that are part of the person table
    Just following the layout of the original interface.
    
    No functionality yet, just appearance.

--- src/persontable.js
+++ src/persontable.js
@@ -2,8 +2,11 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid2';
 import Stack from '@mui/material/Stack';
+import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
 
 export default function PersonTable({data}) {
   return (
@@ -11,38 +14,48 @@ export default function PersonTable({data}) {
       <Typography variant='h6'>Service record, ADM {data.catalogue}</Typography>
       <Card>
         <CardContent>
-          <Grid container columns={7}>
-            <Grid size={1} container alignItems='center'><Typography>Forename, surname</Typography></Grid>
-            <Grid size={3}><TextField fullWidth defaultValue= {data.forename}/></Grid>
-            <Grid size={3}><TextField fullWidth defaultValue= {data.surname}/></Grid>
-            <Grid size={0}/>{ /*!-- Empty element to align row*/ }
+          <Stack direction='row' spacing={2}>
+            <Grid container columns={7}>
+              <Grid size={1} container alignItems='center'><Typography>Forename, surname</Typography></Grid>
+              <Grid size={3}><TextField fullWidth defaultValue= {data.forename}/></Grid>
+              <Grid size={3}><TextField fullWidth defaultValue= {data.surname}/></Grid>
+              <Grid size={0}/>{ /*!-- Empty element to align row*/ }
 
-            <Grid size={1} container alignItems='center'><Typography>Official number</Typography></Grid>
-            <Grid size={3}><TextField fullWidth defaultValue= {data.number}/></Grid>
-            <Grid size={3}/>{ /*!-- Empty element to align row*/ }
+              <Grid size={1} container alignItems='center'><Typography>Official number</Typography></Grid>
+              <Grid size={3}><TextField fullWidth defaultValue= {data.number}/></Grid>
+              <Grid size={3}/>{ /*!-- Empty element to align row*/ }
 
-            <Grid size={1} container alignItems='center'><Typography>Born</Typography></Grid>
-            <Grid size={1}><TextField fullWidth defaultValue= {data.birthDate.getDate()}/></Grid>
-            <Grid size={1}><TextField fullWidth defaultValue= {data.birthDate.getMonth() + 1}/></Grid>
-            <Grid size={1}><TextField fullWidth defaultValue= {data.birthDate.getFullYear()}/></Grid>
-            <Grid size={3}/>{ /*!-- Empty element to align row*/ }
+              <Grid size={1} container alignItems='center'><Typography>Born</Typography></Grid>
+              <Grid size={1}><TextField fullWidth defaultValue= {data.birthDate.getDate()}/></Grid>
+              <Grid size={1}><TextField fullWidth defaultValue= {data.birthDate.getMonth() + 1}/></Grid>
+              <Grid size={1}><TextField fullWidth defaultValue= {data.birthDate.getFullYear()}/></Grid>
+              <Grid size={3}/>{ /*!-- Empty element to align row*/ }
 
-            <Grid size={1} container alignItems='center'><Typography>Birth place, county</Typography></Grid>
-            <Grid size={3}><TextField fullWidth defaultValue= {data.birthPlace}/></Grid>
-            <Grid size={3}><TextField fullWidth defaultValue= {data.birthCounty}/></Grid>
-            <Grid size={0}/>{ /*!-- Empty element to align row*/ }
+              <Grid size={1} container alignItems='center'><Typography>Birth place, county</Typography></Grid>
+              <Grid size={3}><TextField fullWidth defaultValue= {data.birthPlace}/></Grid>
+              <Grid size={3}><TextField fullWidth defaultValue= {data.birthCounty}/></Grid>
+              <Grid size={0}/>{ /*!-- Empty element to align row*/ }
 
-            <Grid size={1} container alignItems='center'><Typography>Occupation</Typography></Grid>
-            <Grid size={3}><TextField fullWidth defaultValue= {data.occupation}/></Grid>
-            <Grid size={3}/>{ /*!-- Empty element to align row*/ }
+              <Grid size={1} container alignItems='center'><Typography>Occupation</Typography></Grid>
+              <Grid size={3}><TextField fullWidth defaultValue= {data.occupation}/></Grid>
+              <Grid size={3}/>{ /*!-- Empty element to align row*/ }
 
-            <Grid size={1} container alignItems='center'><Typography>Discharge date, reason</Typography></Grid>
-            <Grid size={1}><TextField fullWidth defaultValue= {data.dischargeDate.getDate()}/></Grid>
-            <Grid size={1}><TextField fullWidth defaultValue= {data.dischargeDate.getMonth() + 1}/></Grid>
-            <Grid size={1}><TextField fullWidth defaultValue= {data.dischargeDate.getFullYear()}/></Grid>
-            <Grid size={3}><TextField fullWidth defaultValue= {data.dischargeReason}/></Grid>
-            <Grid size={0}/>{ /*!-- Empty element to align row*/ }
-          </Grid>
+              <Grid size={1} container alignItems='center'><Typography>Discharge date, reason</Typography></Grid>
+              <Grid size={1}><TextField fullWidth defaultValue= {data.dischargeDate.getDate()}/></Grid>
+              <Grid size={1}><TextField fullWidth defaultValue= {data.dischargeDate.getMonth() + 1}/></Grid>
+              <Grid size={1}><TextField fullWidth defaultValue= {data.dischargeDate.getFullYear()}/></Grid>
+              <Grid size={3}><TextField fullWidth defaultValue= {data.dischargeReason}/></Grid>
+              <Grid size={0}/>{ /*!-- Empty element to align row*/ }
+            </Grid>
+            <Stack
+              sx={{
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}>
+              <FormControlLabel control={<Checkbox checked={false} onChange={(e)=>{console.log(e)}}/>} label='Error?' labelPlacement='start'/>
+              <Button variant='outlined' onClick={()=>{alert('clicked')}}>Enter</Button>
+            </Stack>
+          </Stack>
         </CardContent>
       </Card>
     </Stack>
