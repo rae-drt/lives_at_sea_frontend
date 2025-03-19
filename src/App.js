e52d293cc9cc1f7812a97a5e74f499aaa1b1a62b
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-03-19 18:36:23 +0000

    Wrap a router around the app
    No functional change for now -- we have one route, from the root
    URL to what we loaded before.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import ServiceRecord from './servicerecord.js';
+import { Routes, Route} from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <ServiceRecord/>
+      <Routes>
+        <Route path='/' element={<ServiceRecord/>}/>
+      </Routes>
     </div>
   );
 }
