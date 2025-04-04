dfbbb54115e6b66401af050029f487770e7fd9de
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-04-04 18:59:39 +0100

    Routes for just /:sailorType
    Route to the first officer record or the first rating record.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Navigate to='/rating/40001'/>}/>
+        <Route path='/rating' element={<Navigate to='/rating/40001'/>}/>
+        <Route path='/officer' element={<Navigate to='/officer/7'/>}/>
         <Route path='/rating/:nameid' element={<Navigate to='main'/>}/>
         <Route path='/officer/:nameid' element={<Navigate to='otherservices'/>}/>
         <Route path='/:sailorType/:nameId/:dataType' element={<Person/>}/>
