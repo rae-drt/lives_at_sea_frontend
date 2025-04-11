b93b1edb079c2f13133572c1f472e7a3eb9a90ce
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-04-11 18:31:17 +0100

    Make the ratings index the default screen

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path={process.env.PUBLIC_URL}>
-          <Route path='' element={<Navigate to='rating/40001'/>}/>{/* lowest rating */}
+          <Route path='' element={<Navigate to='ratings'/>}/>
           <Route path='officers' element={<Navigate to='A'/>}/>
           <Route path='officers/:letter' element={<OfficerIndex/>}/>
           <Route path='ratings' element={<Navigate to='188/5'/>}/>{/* lowest series, lowest piece */}
