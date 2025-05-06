46dfdece5a0fd4d68bd1275adb2883d102c243e4
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-05-06 20:14:27 +0100

    Switch to useQuery for Person records
    This should give me better control, error handling etc
    
    First attempt at using this API so further change in this area is
    likely forthcoming.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import Person from './person';
 import OfficerIndex from './officerindex';
 import RatingsIndex from './ratingsindex';
 import { Routes, Route, Navigate} from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path={process.env.PUBLIC_URL}>
-          <Route path='' element={<Navigate to='ratings'/>}/>
-          <Route path='officers' element={<Navigate to='A'/>}/>
-          <Route path='officers/:letter' element={<OfficerIndex/>}/>
-          <Route path='ratings' element={<Navigate to='188/5'/>}/>{/* lowest series, lowest piece */}
-          <Route path='ratings/:series/:piece' element={<RatingsIndex/>}/>
-          <Route path='rating' element={<Navigate to='40001'/>}/>{/* lowest rating */}
-          <Route path='officer' element={<Navigate to='7'/>}/>{/* lowest officer number */}
-          <Route path='rating/:nameid' element={<Navigate to='main'/>}/>
-          <Route path='officer/:nameid' element={<Navigate to='otherservices'/>}/>
-          <Route path=':sailorType/:nameId/:dataType' element={<Person/>}/>
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path={process.env.PUBLIC_URL}>
+            <Route path='' element={<Navigate to='ratings'/>}/>
+            <Route path='officers' element={<Navigate to='A'/>}/>
+            <Route path='officers/:letter' element={<OfficerIndex/>}/>
+            <Route path='ratings' element={<Navigate to='188/5'/>}/>{/* lowest series, lowest piece */}
+            <Route path='ratings/:series/:piece' element={<RatingsIndex/>}/>
+            <Route path='rating' element={<Navigate to='40001'/>}/>{/* lowest rating */}
+            <Route path='officer' element={<Navigate to='7'/>}/>{/* lowest officer number */}
+            <Route path='rating/:nameid' element={<Navigate to='main'/>}/>
+            <Route path='officer/:nameid' element={<Navigate to='otherservices'/>}/>
+            <Route path=':sailorType/:nameId/:dataType' element={<Person/>}/>
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/person.js
+++ src/person.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useLocation, useNavigate } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import Alert from '@mui/material/Alert';
@@ -24,6 +25,81 @@ const EMPTY_SERVICE_HISTORY = {
   services: [EMPTY_SERVICES, EMPTY_SERVICES],
 };
 
+function mainPersonQF({queryKey}) {
+  const [, {nameId, sailorType}] = queryKey;
+  return new Promise((resolve, reject) => {
+    if(nameId === '0') {
+      resolve(init_data(sailorType));
+    }
+    else if(typeof(nameId) === 'undefined') {
+      reject(new Error());
+    }
+    else {
+      if(sailorType === 'rating') {
+        const fetchData = async() => {
+          const response = await(fetch(process.env.REACT_APP_API_ROOT + 'name?nameid=' + nameId));
+          if(!response.ok) {
+            throw new Error('Bad response: ' + response.status);
+          }
+          return response.json();
+        }
+        resolve(fetchData());
+      }
+      else if(sailorType === 'officer') {
+        const socket = new WebSocket('ws://' + process.env.REACT_APP_QUERYER_ADDR + ':' + process.env.REACT_APP_QUERYER_PORT);
+        socket.onerror = (e) => { reject(e); };
+        socket.onmessage = (e) => {
+          if(e.data === 'NULL') {
+            throw new Error('Bad response');
+          }
+          resolve(JSON.parse(e.data));
+          socket.close();
+        };
+        socket.onopen = () => { socket.send('L@S:Officer:' + nameId) };
+      }
+      else {
+        reject(new Error('Bad sailor type' + sailorType));
+      }
+    }
+  });
+}
+
+function otherServicesQF({queryKey}) {
+  const [, nameId] = queryKey;
+  return new Promise((resolve, reject) => {
+    const socket = new WebSocket('ws://' + process.env.REACT_APP_QUERYER_ADDR + ':' + process.env.REACT_APP_QUERYER_PORT);
+    socket.onerror = (e) => { reject(e); };
+    socket.onmessage = (e) => {
+      if(e.data === 'NULL') {
+        resolve([]);
+      }
+      else {
+        resolve(JSON.parse(e.data));
+      }
+      socket.close();
+    };
+    socket.onopen = () => { socket.send('L@S:OtherServices:' + nameId) };
+  });
+}
+
+function otherDataQF({queryKey}) {
+  const [, nameId] = queryKey;
+  return new Promise((resolve, reject) => {
+    const socket = new WebSocket('ws://' + process.env.REACT_APP_QUERYER_ADDR + ':' + process.env.REACT_APP_QUERYER_PORT);
+    socket.onerror = (e) => { reject(e); };
+    socket.onmessage = (e) => {
+      if(e.data === 'NULL') {
+        resolve([]);
+      }
+      else {
+        resolve(JSON.parse(e.data));
+      }
+      socket.close();
+    };
+    socket.onopen = () => { socket.send('L@S:OtherData:' + nameId) };
+  });
+}
+
 export default function Person() {
   const { sailorType, nameId, dataType } = useParams();
   const { pathname } = useLocation();
@@ -32,81 +108,43 @@ export default function Person() {
   const [serviceRecords, setServiceRecords] = useState(EMPTY_SERVICE_HISTORY);
   const [otherServices, setOtherServices] = useState([]);
   const [otherData, setOtherData] = useState([]);
-  const [fetching, setFetching] = useState(true);
+  const {data: mainPersonQueryData, status: mainPersonQueryStatus} = useQuery({queryKey: ['mainPersonData', {sailorType, nameId}], queryFn: mainPersonQF, refetchOnMount: false, refetchOnWindowFocus: false, refetchOnReconnect: false, staleTime: Infinity});
+  const {data: otherServicesQueryData, status: otherServicesQueryStatus} = useQuery({queryKey: ['otherServicesData', nameId], queryFn: otherServicesQF, refetchOnMount: false, refetchOnWindowFocus: false, refetchOnReconnect: false, staleTime: Infinity});
+  const {data: otherDataQueryData, status: otherDataQueryStatus} = useQuery({queryKey: ['otherData', nameId], queryFn: otherDataQF, refetchOnMount: false, refetchOnWindowFocus: false, refetchOnReconnect: false, staleTime: Infinity});
   useEffect(() => {
-    const fetchData = async() => {
-      setFetching(true);
-      let data;
-      if(nameId === '0') {
-        data = init_data(sailorType);
-        document.title = 'New ' + sailorType;
-        setPersonTableData(data);
-      }
-      else {
-        if(sailorType === 'rating') {
-          const response = await(fetch(process.env.REACT_APP_API_ROOT + 'name?nameid=' + nameId));
-          if(!response.ok) {
-            throw new Error('Bad response: ' + response.status);
-          }
-          data = await(response.json());
-          Object.keys(data.name).forEach((k)=>data.name[k] = data.name[k] ? data.name[k] : '');
-          document.title = catref(data.name);
-          setPersonTableData(data.name);
-          if(_.isEmpty(data.service_history)) setServiceRecords(EMPTY_SERVICE_HISTORY);
-          else {
-            setServiceRecords({reconciled: data.status === 'RECONCILED', services: data.service_history});
-          }
-        }
-        else if(sailorType === 'officer') {
-          const socket = new WebSocket('ws://' + process.env.REACT_APP_QUERYER_ADDR + ':' + process.env.REACT_APP_QUERYER_PORT);
-          socket.onmessage = (e) => {
-            if(e.data === 'NULL') {
-              throw new Error('Bad response');
-            }
-            data = JSON.parse(e.data);
-            document.title = 'Officer #' + officerref(data);
-            setPersonTableData(data);
-            socket.close();
-          };
-          socket.onopen = () => { socket.send('L@S:Officer:' + nameId) };
-        }
-      }
-      setFetching(false);
+    if(mainPersonQueryStatus !== 'success') return;
+    const data = mainPersonQueryData;
+    if(nameId === '0') {
+      document.title = 'New ' + sailorType;
+      setPersonTableData(data);
+      setServiceRecords(EMPTY_SERVICE_HISTORY);
+      return;
     }
-    fetchData();
-  }, [sailorType, nameId]);
+    if(sailorType === 'rating') {
+      Object.keys(data.name).forEach((k)=>data.name[k] = data.name[k] ? data.name[k] : '');
+      setPersonTableData(data.name);
+      document.title = catref(data.name);
+      if(_.isEmpty(data.service_history)) setServiceRecords(EMPTY_SERVICE_HISTORY);
+      else {
+        setServiceRecords({reconciled: data.status === 'RECONCILED', services: data.service_history});
+      }
+    }
+    else if(sailorType === 'officer') {
+      setPersonTableData(data);
+      document.title = 'Officer #' + officerref(data);
+    }
+    else { throw new Error(); }
+  }, [mainPersonQueryData, mainPersonQueryStatus, nameId, sailorType]);
   useEffect(() => {
-    const fetchData = async() => {
-      const socket = new WebSocket('ws://' + process.env.REACT_APP_QUERYER_ADDR + ':' + process.env.REACT_APP_QUERYER_PORT);
-      socket.onmessage = (e) => {
-        if(e.data === 'NULL') {
-          setOtherData([]);
-        }
-        else {
-          setOtherData(JSON.parse(e.data));
-        }
-        socket.close();
-      };
-      socket.onopen = () => { socket.send('L@S:OtherData:' + nameId) };
-    };
-    fetchData();
-  }, [nameId]);
+    if(otherServicesQueryStatus === 'success') {
+      setOtherServices(otherServicesQueryData);
+    }
+  }, [otherServicesQueryData, otherServicesQueryStatus]);
   useEffect(() => {
-    const fetchData = async() => {
-      const socket = new WebSocket('ws://' + process.env.REACT_APP_QUERYER_ADDR + ':' + process.env.REACT_APP_QUERYER_PORT);
-      socket.onmessage = (e) => {
-        if(e.data === 'NULL') {
-          setOtherServices([]);
-        }
-        else {
-          setOtherServices(JSON.parse(e.data));
-        }
-        socket.close();
-      };
-      socket.onopen = () => { socket.send('L@S:OtherServices:' + nameId) };
-    };
-    fetchData();
-  }, [nameId]);
+    if(otherDataQueryStatus === 'success') {
+      setOtherData(otherDataQueryData);
+    }
+  }, [otherDataQueryData, otherDataQueryStatus]);
 
   const theme = createTheme({
     typography: {
@@ -121,13 +159,16 @@ export default function Person() {
   ) {
     return (<Alert severity='error'>Bad location: {pathname}</Alert>);
   }
-  else if(((typeof serviceRecords) === 'undefined') || ((typeof personTableData) === 'undefined')) {
+  else if((typeof(serviceRecords) === 'undefined') || (typeof(personTableData) === 'undefined')) {
     return (<Stack height='100vh' width='100vw' alignItems='center' justifyContent='center'><CircularProgress size='50vh'/></Stack>);
+  }
+  else if(mainPersonQueryStatus === 'error' || otherServicesQueryStatus === 'error' || otherDataQueryStatus === 'error') {
+    return (<Alert severity='error'>Error fetching data</Alert>);
   }
   else {
     //TODO: Assuming that we get an empty array when there are no service records, and hence can check for length == 0
     return (
-      <LoadingContext value={fetching}>
+      <LoadingContext value={mainPersonQueryStatus === 'pending' || otherServicesQueryStatus === 'pending' || otherDataQueryStatus === 'pending'}>
         <Stack direction='row' spacing={2} alignItems='center' justifyContent='space-around' width={0.95}>
           <ThemeProvider theme={theme}>
             <Stack sx={{alignItems: 'center', justifyContent: 'space-evenly'}} spacing={2}>
