f095a77d3ca34c253069d10bf0e6f1ed18938928
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-05-07 17:38:26 +0100

    Add a means of viewing the 'context' tables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Person from './person';
 import OfficerIndex from './officerindex';
 import RatingsIndex from './ratingsindex';
+import SimpleEditor from './simpleeditor';
 import { Routes, Route, Navigate} from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -23,6 +24,17 @@ function App() {
             <Route path='rating/:nameid' element={<Navigate to='main'/>}/>
             <Route path='officer/:nameid' element={<Navigate to='otherservices'/>}/>
             <Route path=':sailorType/:nameId/:dataType' element={<Person/>}/>
+            <Route path='sources' element={<SimpleEditor table='sources' primary='sourceid'/>}/>
+            <Route path='ships' element={<SimpleEditor table='ships' primary='shipid'/>}/>
+            <Route path='battles' element={<SimpleEditor table='battlehonours' primary='battleid'/>}/>
+            <Route path='counties' element={<SimpleEditor table='counties' primary='countyId'/>}/>
+            <Route path='places' element={<SimpleEditor table='places' primary='placeId'/>}/>
+            <Route path='ratingstable' element={<SimpleEditor table='ratings' primary='ratingid'/>}/>
+            <Route path='ratingsauto' element={<SimpleEditor table='ratingsauto' primary='ratingautoid'/>}/>
+            {/*TODO: What do we do with these awkward cases?
+              <Route path='acronyms' element={<SimpleEditor table='acronyms' primary='acronym'/>}/> -- tricky case, there is no independent primary key, and the actual primary (acronym) is a non-integer and needs to be visible and editable
+              <Route path='countries' element={<SimpleEditor table='countries' primary='country'/>}/> -- this one only has the country field*
+            */}
           </Route>
         </Routes>
       </QueryClientProvider>
---
+++ src/simpleeditor.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import ReactDOM from 'react-dom';
+import DataTable from './datatable';
+import { LoadingContext } from './loadingcontext';
+import { Alert, CircularProgress, Stack } from '@mui/material';
+import { useGridApiRef } from '@mui/x-data-grid';
+
+function queryFn({queryKey}) {
+  const [, table] = queryKey;
+  return new Promise((resolve, reject) => {
+    const socket = new WebSocket('ws://' + process.env.REACT_APP_QUERYER_ADDR + ':' + process.env.REACT_APP_QUERYER_PORT);
+    socket.onerror = (e) => { reject(e); };
+    socket.onmessage = (e) => {
+      if(e.data === 'NULL') {
+        resolve([]);
+      }
+      else {
+        resolve(JSON.parse(e.data));
+      }
+      socket.close();
+    };
+    socket.onopen = () => { socket.send('L@S:SimpleData:' + table) };
+  });
+}
+
+export default function SimpleEditor({table, primary}) {
+  const apiRef = useGridApiRef();
+  const [rows, setRows] = useState([]);
+  const [fields, setFields] = useState([]);
+  const { data: queryData, status: queryStatus } = useQuery({
+    queryKey: ['simpleEditor', table],
+    queryFn: queryFn,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    staleTime: Infinity,
+  });
+  useEffect(() => {
+    if(queryStatus === 'success') {
+      setRows(queryData);
+    }
+  }, [queryData, queryStatus]);
+  useEffect(() => {
+    if(rows && rows.length) setFields(Object.keys(rows[0]));
+    else setFields([]);
+  }, [rows])
+  useEffect(() => { //re https://github.com/mui/mui-x/issues/10578. Unfortunately the 3rd solution requires a non-basic version of DataGrid so we live with the jump.
+    const timeoutId = setTimeout(()=>{
+      ReactDOM.flushSync(()=>{
+        apiRef.current.autosizeColumns({includeOutliers: true, includeHeaders: true, expand: true});
+      });
+    },1000);
+   return () => { clearInterval(timeoutId); }
+  }, [rows, apiRef]);
+
+  if(fields.length) {
+    const columns: GridColDef[] = fields.map((e) => ({
+      field: e,
+      headerName: e[0].toUpperCase() + e.slice(1),
+      editable: e !== primary,
+    }));
+    return (
+      <LoadingContext value={queryStatus==='pending'}>
+        <DataTable
+          apiRef={apiRef}
+          rows={rows}
+          columns={columns}
+          primary={primary}
+          onChange={setRows}
+        />
+      </LoadingContext>
+    );
+  }
+  else if(queryStatus === 'error') {
+    return (<Alert severity='error'>Error fetching data</Alert>);
+  }
+  else {
+    return (<Stack height='100vh' width='100vw' alignItems='center' justifyContent='center'><CircularProgress size='50vh'/></Stack>);
+  } 
+}
