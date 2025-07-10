0fd7b4a9dd43f24e703824f271f2c808688a194c
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-07-10 16:23:47 +0100

    Dumping current testing state prior to attempting a switch to nextjs

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,15 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-// from https://remarkablemark.org/blog/2025/02/02/fix-jest-errors-in-react-router-7-upgrade/
+//from https://remarkablemark.org/blog/2025/02/02/fix-jest-errors-in-react-router-7-upgrade/
 import { TextEncoder } from 'util';
 global.TextEncoder = TextEncoder;
+
+//re https://github.com/socketio/socket.io/issues/5064#issuecomment-2217149231
+import { TransformStream } from 'node:stream/web';
+global.TransformStream = TransformStream;
+/*
+module.exports = {
+  testEnvironment: 'jest-fixed-jsdom',
+}
+*/
