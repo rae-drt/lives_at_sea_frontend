55a0699fd0cd461dc810b475caa9b14025feee2a
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-07-15 19:25:42 +0100

    More sophisticated config
    No linting in production builds, throw an error in unexpected
    conditions.
    
    re https://vite.dev/config/

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,21 @@ import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    outDir: 'build',
-  },
-  plugins: [react(), eslint()],
+export default defineConfig(({command, mode}) => {
+  if(command === 'build' || mode === 'production') {
+    return {
+      build: {
+        outDir: 'build',
+      },
+      plugins: [react()],
+    }
+  }
+  else if(command === 'serve') {
+    return {
+      plugins: [react(), eslint()],
+    }
+  }
+  else {
+    throw Error('Check vite.config.js');
+  }
 })
