5991a221c8e9e7a3957120a4ace8450e194de37d
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-07-17 13:39:02 +0100

    Try adding Authenticator tag

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 //Per https://stackoverflow.com/a/78608514
 const router = createBrowserRouter(createRoutesFromElements(<Route path='*' element={<App />} />));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Authenticator hideSignUp>
+      <RouterProvider router={router}/>
+    </Authenticator>
   </React.StrictMode>
 );
