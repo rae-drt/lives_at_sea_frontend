5141212e0f956aedbb908b93b193dfdb91416b4b
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-07-23 13:36:11 +0100

    Basic amplify wrapping
    Adds the gubbins that Amplify seems to need and a little wrapping
    in the application itself -- but no amplify-specific functionality
    has been added to the application yet.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 //Per https://stackoverflow.com/a/78608514
 const router = createBrowserRouter(createRoutesFromElements(<Route path='*' element={<App />} />));
