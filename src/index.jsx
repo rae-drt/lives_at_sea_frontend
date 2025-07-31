457555dab24cdd92b227559822977bb465bffc81
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-07-31 14:20:53 +0100

    Add authenticator

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import App from './App';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
 import outputs from '../amplify_outputs.json';
 
 Amplify.configure(outputs);
@@ -14,6 +15,8 @@ const router = createBrowserRouter(createRoutesFromElements(<Route path='*' elem
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Authenticator hideSignUp>
+      <RouterProvider router={router}/>
+    </Authenticator>
   </React.StrictMode>
 );
