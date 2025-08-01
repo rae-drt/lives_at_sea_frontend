74d8d5904e8fa63e1b719c5b25630ec059edbfd1
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-08-01 18:32:17 +0100

    WIP

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,10 @@ import OfficerIndex from './officerindex.jsx';
 import SimpleEditor from './simpleeditor.jsx';
 import Person from './person.jsx';
 
+function BadRoute() {
+  throw Error(`Bad route: ${window.location.pathname}`);
+}
+
 export const routes = (
   <Routes>
     <Route path='/'>
@@ -12,11 +16,12 @@ export const routes = (
       <Route path='officers/:letter' element={<OfficerIndex/>}/>
       <Route path='ratings' element={<Navigate to='5'/>}/>{/* lowest piece */}
       <Route path='ratings/:piece' element={<RatingsIndex/>}/>
-      <Route path='rating' element={<Navigate to='40001'/>}/>{/* lowest rating */}
-      <Route path='officer' element={<Navigate to='7'/>}/>{/* lowest officer number */}
-      <Route path='rating/:nameid' element={<Navigate to='main'/>}/>
-      <Route path='officer/:nameid' element={<Navigate to='otherservices'/>}/>
-      <Route path=':sailorType/:nameId/:dataType' element={<Person/>}/>
+      <Route path='person' element={<Navigate to='/person/rating'/>}/>{/* default to ratings */}
+      <Route path='person/rating' element={<Navigate to='40001'/>}/>{/* lowest rating */}
+      <Route path='person/officer' element={<Navigate to='7'/>}/>{/* lowest officer number */}
+      <Route path='person/rating/:nameid' element={<Navigate to='main'/>}/>
+      <Route path='person/officer/:nameid' element={<Navigate to='otherservices'/>}/>
+      <Route path='person/:sailorType/:nameId/:dataType' element={<Person/>}/>
       <Route path='sources' element={<SimpleEditor table='sources' primary='sourceid'/>}/>
       <Route path='ships' element={<SimpleEditor table='ships' primary='shipid'/>}/>
       <Route path='battles' element={<SimpleEditor table='battlehonours' primary='battleid'/>}/>
@@ -29,5 +34,6 @@ export const routes = (
         <Route path='countries' element={<SimpleEditor table='countries' primary='country'/>}/> -- this one only has the country field*
       */}
     </Route>
+      <Route path='*' element={<BadRoute/>}/>
   </Routes>
 );
