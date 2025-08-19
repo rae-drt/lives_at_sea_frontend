313273f5214af997c59857432cabbcf2bd9c5e8a
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-08-19 16:57:56 +0100

    Demote no-unreachable to warning

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default defineConfig([
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "warn",
       "no-unused-vars": "warn",
+      "no-unreachable": "warn",
     },
   },
 ]);
