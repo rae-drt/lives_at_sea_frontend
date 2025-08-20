c07507244327cefa7c7f81f863d1265330dadd75
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-08-20 17:40:34 +0100

    Mechanism to drop in alternative index.jsx
    Handy for wrapping the app in different ways for different
    purposes.
    
    index.jsx becomes an overridable symlink created on the fly, so
    remove it from version control.
    
    Assumption is that the alternative implementations will not be
    committed, so define a .gitignore pattern for them.

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import { unlink, symlink } from 'node:fs/promises';
 
 // https://vite.dev/config/
 export default defineConfig(({command, mode}) => {
+  function link_index() {
+    if(command === 'serve') {
+      symlink('alt_dev_index.jsx', 'src/index.jsx', 'file');
+    }
+    else {
+      symlink('real_index.jsx', 'src/index.jsx', 'file');
+    }
+  }
+
+  //separate implementation so that it is crystal clear what happens in production
+  function production_link_index() {
+    symlink('real_index.jsx', 'src/index.jsx', 'file');
+  }
+
   if(command === 'build' || mode === 'production') {
     return {
       build: {
         outDir: 'build',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        {
+          name: 'prebuild_production',
+          buildStart(options) {
+            //delete src/index.jsx if it exists. either way, create a link to a suitable implementation.
+            unlink('src/index.jsx').then(production_link_index,
+                                         production_link_index);
+          },
+        },
+      ],
     }
   }
   else if(command === 'serve') {
     return {
-      plugins: [react(), eslint()],
+      plugins: [
+        react(),
+        {
+          name: 'prebuild',
+          buildStart(options) {
+            //delete src/index.jsx if it exists. either way, create a link to a suitable implementation.
+            unlink('src/index.jsx').then(link_index,
+                                         link_index);
+          },
+        },
+        eslint()
+      ],
     }
   }
   else {
