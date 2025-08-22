851abbad688145408405a119bf439b820380ad23
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-08-22 16:30:35 +0100

    clone tests

--- src/datatable.jsx
+++ src/datatable.jsx
@@ -126,7 +126,7 @@ export function DataTable(props) {
         {insertionButtons}
         <Tooltip title='Delete row' placement='top' followCursor arrow>
           <span>
-            <IconButton sx={sx} fontSize='inherit' color='primary' onClick={()=>{
+            <IconButton sx={sx} fontSize='inherit' color='primary' data-testid='deleteRowButton' onClick={()=>{
               const newRows = [];
               for(const x of rows) {
                 if(x[primary] === row[primary]) continue;
