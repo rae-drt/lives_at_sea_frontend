d22848f4d9a69272b309c10aa9e8cd8e668ee3c3
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-12-04 17:10:23 +0000

    Update eslint config
    Includes adding linting for the hooks, which I had not realised
    that I was missing.
    
    Also added linting on build.

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,27 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
+  {
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    }
+  },
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  reactHooks.configs.flat.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
   {
     rules: {
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "warn",
-      "no-unused-vars": "warn",
+      "react/prop-types": "off",
+      "react-hooks/exhaustive-deps": "error",
+      "no-unused-vars": "off",
       "no-unreachable": "warn",
     },
   },
--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default defineConfig(({command, mode}) => {
                                          production_link_index);
           },
         },
+        eslint()
       ],
     }
   }
